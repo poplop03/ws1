f1482f96d889287ef0b55adf28197c7395e19ee8
poplop03 <thienductang@gmail.com>
2024-12-18 16:38:36 +0700

    5

--- build/ws1botctrl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/duc/ROS1/ws1/src/ws1botctrl
-# Build directory: /home/duc/ROS1/ws1/build/ws1botctrl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
